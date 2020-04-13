eaf9c1ac00d0673259f993373a2d599ecac98321
pychecker <pychecker@126.com>
2020-04-13 15:28:52 +0800

    sys

---
+++ oracle/phporacle.php
@@ -0,0 +1,13 @@
+<?php
+
+//phpinfo();
+//exit;
+  $conn = ocilogon('system','demored','127.0.0.1:1521/orcl');
+  if (!$conn){
+    $Error = oci_error();
+    print htmlentities($Error['message']);
+    exit;
+  }else{
+    echo "Connected Oracle Successd!"."<br>";
+  }
+  
