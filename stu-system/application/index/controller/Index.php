0e9cb062cd2b5350c1c820557a60b664329773d5
pychecker <pychecker@126.com>
2020-09-19 20:40:57 +0800

    update

---
+++ stu-system/application/index/controller/Index.php
@@ -0,0 +1,22 @@
+<?php
+namespace app\index\controller;
+use think\Controller;
+class Index extends Controller
+{
+    //主要做一个导航
+    public function index()
+    {
+        return $this -> fetch();
+    }
+
+    //上传大文件
+    public function upload_big_excel(){
+        if(request()->isPost()){
+            echo "hello world";
+            exit;
+
+        }else{
+            return $this -> fetch();
+        }
+    }
+}
--- stu-system/public/index.php
+++ stu-system/public/index.php
@@ -2,7 +2,7 @@
 // +----------------------------------------------------------------------
 // | ThinkPHP [ WE CAN DO IT JUST THINK ]
 // +----------------------------------------------------------------------
-// | Copyright (c) 2006-2018 http://thinkphp.cn All rights reserved.
+// | Copyright (c) 2006-2016 http://thinkphp.cn All rights reserved.
 // +----------------------------------------------------------------------
 // | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
 // +----------------------------------------------------------------------
@@ -10,9 +10,17 @@
 // +----------------------------------------------------------------------
 
 // [ 应用入口文件 ]
-namespace think;
 
-require __DIR__ . '/../vendor/autoload.php';
 
-// 执行HTTP应用并响应
-(new App())->http->run()->send();
+define('APP_HOST', 'http://'.$_SERVER['HTTP_HOST']);  # http://127.0.0.1:8080
+
+define('APP_URL', substr($_SERVER['SCRIPT_NAME'], 0, -10)); # /home/wwwroot/www.test.com
+
+define('APP_DIR', __DIR__ ); // windows: E:\home\wwwroot\api.seqier.com  linux:/home/wwwroot/api.seqier.com  //同系统ROOT_PATH
+
+
+
+// 定义应用目录
+define('APP_PATH', __DIR__ . '/../application/');
+// 加载框架引导文件
+require __DIR__ . '/../thinkphp/start.php';
